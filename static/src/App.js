ba32dacb45a349e4df2288c7f7655f66adf4d2de
susisonnenschein <susannasuchan@web.de>
2025-04-16 09:31:05 +0200

    GPTW 2025

--- static/src/App.js
+++ static/src/App.js
@@ -1,29 +1,30 @@
-import React, { Fragment, useEffect, useState } from "react"
-import { requestConfluence } from "@forge/bridge"
+import React, { Fragment, useEffect, useState } from "react";
+import { requestConfluence } from "@forge/bridge";
 
 async function fetchUser() {
-  const res = await requestConfluence("/wiki/rest/api/user/current")
+  const res = await requestConfluence("/wiki/rest/api/user/current");
   if (!res.ok) {
-    throw new Exception("Could not fetch current username")
+    throw new Exception("Could not fetch current username");
   }
-  return await res.json()
+  return await res.json();
 }
 
 function Location({ officeLocation }) {
   if (officeLocation === "mainz") {
     return (
       <div id="location">
-        Rheinstraße 4 C<br/>
+        Rheinstraße 4 C<br />
         55116 Mainz, Germany
       </div>
-    )
+    );
   }
   return (
     <div id="location">
-      Aschauer Straße 30<br/>
+      Aschauer Straße 30
+      <br />
       81549 München, Germany
     </div>
-  )
+  );
 }
 
 function Signature(props) {
@@ -36,12 +37,14 @@ function Signature(props) {
         style={{ height: "1px", backgroundColor: "#ccc", border: "none" }}
       />
 
-      <div style={{ fontSize: "8pt", fontFamily: "sans-serif", color: "black" }}>
+      <div
+        style={{ fontSize: "8pt", fontFamily: "sans-serif", color: "black" }}
+      >
         <p style={{ margin: "0pt 1pt 0pt" }}>
-          <b>Beste Arbeitgeber ITK 2023 - 1. Platz für QAware</b>
+          <b>Beste Arbeitgeber ITK 2025 - 1. Platz für QAware</b>
           <br />
           ausgezeichnet von&nbsp;
-          <a href="https://www.qaware.de/news/great-place-to-work-2023-triple-fuer-qaware/">
+          <a href="https://www.qaware.de/news/gptw-2025/">
             Great Place to Work
           </a>
         </p>
@@ -53,14 +56,19 @@ function Signature(props) {
         />
         <p style={{ margin: "0pt 1pt 8pt" }}>
           {props.title} {props.name}
-          <br/>
-          {props.roles.split("\n").map(role => <Fragment>{role}<br/></Fragment>)}
+          <br />
+          {props.roles.split("\n").map((role) => (
+            <Fragment>
+              {role}
+              <br />
+            </Fragment>
+          ))}
         </p>
         <p style={{ margin: "0pt 1pt 0pt" }}>
           QAware GmbH
           <br />
           <Location officeLocation={props.officeLocation} />
-          {props.phone !== "" ? (<div>Mobil {props.phone}</div>) : null}
+          {props.phone !== "" ? <div>Mobil {props.phone}</div> : null}
           <a href={`mailto:${props.email}`}>{props.email}</a>
           <br />
           <a href="https://www.qaware.de">www.qaware.de</a>
@@ -75,9 +83,12 @@ function Signature(props) {
         style={{ height: "1px", backgroundColor: "#ccc", border: "none" }}
       />
 
-      <div style={{ fontSize: "7pt", fontFamily: "sans-serif", color: "black" }}>
+      <div
+        style={{ fontSize: "7pt", fontFamily: "sans-serif", color: "black" }}
+      >
         <p style={{ margin: "0pt 1pt 14pt" }}>
-          Geschäftsführer: Dr. Josef Adersberger, Mario-Leander Reimer, Michael Rohleder, Michael Stehnken
+          Geschäftsführer: Dr. Josef Adersberger, Mario-Leander Reimer, Michael
+          Rohleder, Michael Stehnken
           <br />
           Registergericht: München
           <br />
@@ -86,57 +97,69 @@ function Signature(props) {
         </p>
       </div>
     </div>
-  )
+  );
 }
 
 function copyDataToClipboard(data, formats = ["text/plain"]) {
   function listener(e) {
     for (const format of formats) {
-      e.clipboardData.setData(format, data)
+      e.clipboardData.setData(format, data);
     }
-    e.preventDefault()
+    e.preventDefault();
   }
-  document.addEventListener("copy", listener)
-  document.execCommand("copy")
-  document.removeEventListener("copy", listener)
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
 }
 
 function App() {
-  const [user, setUser] = useState(null)
-  const [title, setTitle] = useState("")
-  const [roles, setRoles] = useState("")
-  const [location, setLocation] = useState("munich")
-  const [phone, setPhone] = useState("")
-  const [hasBeenCopied, setHasBeenCopied]  = useState(false)
-  const [email, setEmail] = useState("")
-  const [userName, setUserName] = useState("")
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [roles, setRoles] = useState("");
+  const [location, setLocation] = useState("munich");
+  const [phone, setPhone] = useState("");
+  const [hasBeenCopied, setHasBeenCopied] = useState(false);
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
 
   useEffect(async () => {
-    const user = await fetchUser("fetchUser")
-    setUser(user)
-    setEmail(user.email)
-    setUserName(user.displayName)
-  }, [])
+    const user = await fetchUser("fetchUser");
+    setUser(user);
+    setEmail(user.email);
+    setUserName(user.displayName);
+  }, []);
 
   function copySignature() {
-    copyDataToClipboard(
-      document.getElementById("signature").innerHTML,
-      ["text/html", "text/plain"]
-    )
-    setHasBeenCopied(true)
-    setTimeout(() => setHasBeenCopied(false), 2000)
+    copyDataToClipboard(document.getElementById("signature").innerHTML, [
+      "text/html",
+      "text/plain",
+    ]);
+    setHasBeenCopied(true);
+    setTimeout(() => setHasBeenCopied(false), 2000);
   }
 
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <div id="signature-input" style={{ border: "1px solid #fec900", borderRadius: "3px" }}>
-          <div style={{ backgroundColor: "#fec900", color: "#333", padding: "8px", fontWeight: "bold" }}>
+        <div
+          id="signature-input"
+          style={{ border: "1px solid #fec900", borderRadius: "3px" }}
+        >
+          <div
+            style={{
+              backgroundColor: "#fec900",
+              color: "#333",
+              padding: "8px",
+              fontWeight: "bold",
+            }}
+          >
             Deine Angaben
           </div>
           <div style={{ padding: "8px" }}>
             <p>
-              <label for="user-title" style={{ marginRight: "1em" }}>Dein akademischer Titel:</label>
+              <label for="user-title" style={{ marginRight: "1em" }}>
+                Dein akademischer Titel:
+              </label>
               <select
                 name="user-title"
                 id="user-title"
@@ -173,7 +196,9 @@ function App() {
               />
             </p>
             <p>
-              <label for="user-location" style={{ marginRight: "1em" }}>Dein Standort:</label>
+              <label for="user-location" style={{ marginRight: "1em" }}>
+                Dein Standort:
+              </label>
               <select
                 name="user-location"
                 id="user-location"
@@ -209,8 +234,22 @@ function App() {
             </p>
           </div>
         </div>
-        <div id="signature-output" style={{ marginLeft: "2em", border: "1px solid #0593d8", borderRadius: "3px" }}>
-          <div style={{ backgroundColor: "#0593d8", color: "white", padding: "8px", fontWeight: "bold" }}>
+        <div
+          id="signature-output"
+          style={{
+            marginLeft: "2em",
+            border: "1px solid #0593d8",
+            borderRadius: "3px",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "#0593d8",
+              color: "white",
+              padding: "8px",
+              fontWeight: "bold",
+            }}
+          >
             Deine generierte QAmail-Signatur 🙂
           </div>
           <div style={{ padding: "8px" }}>
@@ -244,7 +283,7 @@ function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
